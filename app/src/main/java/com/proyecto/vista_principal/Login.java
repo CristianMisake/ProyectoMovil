463a1a53d24c42143cf6814825412e3e0dc7664f
Cristian Grondona <cristiangrondon@gmail.com>
2021-04-14 20:51:27 -0500

    enviar a registro

--- app/src/main/java/com/proyecto/vista_principal/Login.java
+++ app/src/main/java/com/proyecto/vista_principal/Login.java
@@ -2,7 +2,9 @@ package com.proyecto.vista_principal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.proyecto.R;
 
@@ -12,5 +14,11 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void enviarRegistro(View view) {
+        //Invocar a la tercera actividad
+        Intent intent = new Intent(this, Registro.class);
+        startActivity(intent);
     }
 }
